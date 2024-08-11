196f1c1600d82fe5eb34d699b97e601c5be944fd
Cachumbin <simoncarreno2006@gmail.com>
2024-08-11 15:26:56 -0500

    feat: Saved the todos in the local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
+import { useEffect } from 'react'
 import TodoForm from './Components/TodoForm'
 import TodoList from './Components/TodoList'
 
 const App = () => {
-  const [todos, setTodos] = useState([])
+
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if(localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(()=> {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
     setTodos(currentTodos => {
