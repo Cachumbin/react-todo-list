3d457f115a89b7e0eb72dd246423e8119312b263
Cachumbin <simoncarreno2006@gmail.com>
2024-08-13 14:14:33 -0500

    fix: Label tag now points to the component checkbox

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const TodoItem = ({ completed, id, title, toggleTodo, deleteTodo }) => {
   return (
     <li /*key={todo.id}*/>
-          <label htmlFor="check-item" className='item-label'>
-            <input type="checkbox" id='check-item' checked={completed} onChange={e => toggleTodo(id, e.target.checked)}/>
+          <label htmlFor={id} className='item-label'>
+            <input type="checkbox" id={id} checked={completed} onChange={e => toggleTodo(id, e.target.checked)}/>
             {title}
           </label>
           <button onClick={() => deleteTodo(id)} className='btn btn-danger'>Delete</button>
