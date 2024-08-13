8a38b10b2323a9749cf4d6b9b488c46d89a4e43a
Cachumbin <simoncarreno2006@gmail.com>
2024-08-13 14:16:00 -0500

    style: Fix the margin of li checkbox

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -4,7 +4,7 @@ const TodoItem = ({ completed, id, title, toggleTodo, deleteTodo }) => {
   return (
     <li /*key={todo.id}*/>
           <label htmlFor={id} className='item-label'>
-            <input type="checkbox" id={id} checked={completed} onChange={e => toggleTodo(id, e.target.checked)}/>
+            <input type="checkbox" id={id} checked={completed} onChange={e => toggleTodo(id, e.target.checked)} className='item-checkbox'/>
             {title}
           </label>
           <button onClick={() => deleteTodo(id)} className='btn btn-danger'>Delete</button>
